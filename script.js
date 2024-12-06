a4780dbeff210b00d5695fa0454d8856c786bebc
Claudia Medina <claudiamedina_1@hotmail.com>
2024-12-06 11:26:42 -0400

    comentarios hechos:)

--- script.js
+++ script.js
@@ -28,6 +28,18 @@ console.log(usuario.direccion.avenida)
 
 */
 
+/*primer paso
+const createSongComponent = (song) => {
+    const component = document.createElement('div')
+    component.setAttribute('class', 'playlist__canciones__canciones__cancion')
+
+    component.innerHTML = `
+    <img class="playlist__canciones__canciones__cancion__foto" src="${song.image.url}" alt="">
+    <div class="playlist__canciones__canciones__cancion__tituloyautor">
+        <h2 class="playlist__canciones__canciones__cancion__tituloyautor__titulo">${song.title}</h2>
+        <p class="playlist__canciones__canciones__cancion__tituloyautor__autor">${song.author}</p>
+
+    </div>*/
 const createSongComponent = (song) => {
     const component = document.createElement('div')
     component.setAttribute('class', 'playlist__canciones__canciones__cancion')
@@ -41,12 +53,22 @@ const createSongComponent = (song) => {
     </div>
     
     `
+/*quinto paso
+setInteractiveComponent(component, song)
+    return component*/
 
     setInteractiveComponent(component, song)
     return component
     
 }
 
+/*cuarto paso
+const setInteractiveComponent = (component, song) => {
+    component.addEventListener('click', () => {
+        document.getElementById('audio').setAttribute('src', song.audio.url)
+        document.getElementById('imagen').setAttribute('src', song.image.url)
+        document.getElementById('title-song').innerHTML = song.title
+        document.getElementById('author-song').innerHTML = song.author*/
 const setInteractiveComponent = (component, song) => {
     component.addEventListener('click', () => {
         document.getElementById('audio').setAttribute('src', song.audio.url)
@@ -55,7 +77,15 @@ const setInteractiveComponent = (component, song) => {
         document.getElementById('author-song').innerHTML = song.author
     })
 }
+/*segundo paso 
+axios.get('https://api.institutoalfa.org/api/songs')
+.then(function (response) {
+    const songsInfo = response.data.songs
+    const contenedor = document.getElementById('container-song')
 
+    songsInfo.map((song) => {
+    const songComponent = createSongComponent(song)
+    contenedor.appendChild(songComponent)*/
 axios.get('https://api.institutoalfa.org/api/songs')
 .then(function (response) {
     const songsInfo = response.data.songs
